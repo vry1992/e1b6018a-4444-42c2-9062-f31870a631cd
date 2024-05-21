9587b33a1e38a5b4022d2ee1a72b1345ccc69667
Vitalii Rybachuk <vitalii.rybachuk@infopulse.com>
2024-05-21 16:00:48 +0300

    update

--- src/map/Map.tsx
+++ src/map/Map.tsx
@@ -1,13 +1,12 @@
-import L, { Control, LatLng } from 'leaflet';
+import L, { LatLng } from 'leaflet';
 import 'leaflet/dist/leaflet.css';
-import { FC, PropsWithChildren, useEffect, useRef, useState } from 'react';
-import { LayersControl, MapContainer, TileLayer, useMap } from 'react-leaflet';
+import { FC, PropsWithChildren, useEffect, useState } from 'react';
+import { MapContainer, Marker, TileLayer } from 'react-leaflet';
 
 import 'leaflet-routing-machine';
 import 'leaflet-routing-machine/dist/leaflet-routing-machine.css';
 import { useSearchParams } from 'react-router-dom';
 
-// Base map tile:
 const maps = {
   base: 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
 };
@@ -15,7 +14,7 @@ const maps = {
 export const Container: FC<PropsWithChildren> = ({ children }) => {
   return (
     <MapContainer
-      center={[37.0902, -95.7129]}
+      center={[46.4314118, 30.7008806]}
       zoom={3}
       zoomControl={false}
       style={{ height: '100vh', width: '100%', padding: 0 }}>
@@ -24,38 +23,16 @@ export const Container: FC<PropsWithChildren> = ({ children }) => {
   );
 };
 
+const icon = new L.DivIcon({
+  html: `<div style="position: relative;background: red; width: 10px; height: 10px; border-radius: 5px"></div>`,
+  iconSize: [10, 10],
+  className: 'leaflet-div-icon-custom',
+});
+
 export const Map = () => {
   const [params] = useSearchParams();
-  // Save map instance to state here:
-  const map = useMap();
-  // Save routing machine instance to state here:
-  const [routingMachine, setRoutingMachine] = useState(null);
 
   const [points, setPoints] = useState<LatLng[]>([]);
-
-  // Routing machine ref
-  const RoutingMachineRef = useRef<Control | null>(null);
-
-  // Create the routing-machine instance:
-  useEffect(() => {
-    // @ts-ignore
-    RoutingMachineRef.current = L.Routing.control({
-      position: 'topleft',
-      autoRoute: false,
-      // @ts-ignore
-      lineOptions: {
-        styles: [
-          {
-            color: '#757de8',
-          },
-        ],
-      },
-      waypoints: [],
-    });
-    console.log(RoutingMachineRef.current);
-    // @ts-ignore
-    setRoutingMachine(RoutingMachineRef.current);
-  }, [map]);
 
   useEffect(() => {
     const rawParams = params.get('p');
@@ -67,26 +44,17 @@ export const Map = () => {
     setPoints(pos);
   }, [params]);
 
-  // // Set waypoints when start and end points are updated:
-  useEffect(() => {
-    if (routingMachine) {
-      //@ts-ignore
-      routingMachine.addTo(map);
-      //@ts-ignore
-      routingMachine.setWaypoints(points);
-    }
-  }, [routingMachine, points]);
-
-  // Update start and end points on button click:
-
   return (
-    <LayersControl position="topright">
-      {/* <LayersControl.BaseLayer checked name="Map"> */}
+    <>
       <TileLayer
         attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
         url={maps.base}
       />
-      {/* </LayersControl.BaseLayer> */}
-    </LayersControl>
+      <>
+        {points.map((latlng) => (
+          <Marker position={latlng} icon={icon} />
+        ))}
+      </>
+    </>
   );
 };
